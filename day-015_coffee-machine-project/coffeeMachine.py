a6faa38211ce6f5276e6258c1f3a2ebc5d260da0
southPaw <dangargiullo@gmail.com>
2022-04-27 17:41:19 -0400

    Upload finished Coffee Machine project to WIP branch.

--- day-015_coffee-machine-project/coffeeMachine.py
+++ day-015_coffee-machine-project/coffeeMachine.py
@@ -1,4 +1,4 @@
-# Write a program that simulates a coffee machine in an office.
+# Write a program that simulates a coffee machine.
 
 supplies = {
     "water": 10_000,
@@ -9,19 +9,19 @@ supplies = {
 }
 
 MENU = {
-    "coffee": {
+    "1": {  # coffee
         "ingredients": {
             "water": 10,
             "beans": 10
         },
-        "cost": 1.00
+        "price": 1.00
     },
-    "tea": {
+    "2": {  # tea
         "ingredients": {
             "water": 12,
             "tea-leaves": 9
         },
-        "cost": .75
+        "price": .75
     }
 }
 
@@ -31,6 +31,7 @@ def enough_supplies(ingredients):
         if supplies[item] < ingredients[item]:
             print(f"Sorry, we're out of {item}!")
             return False
+    return True
 
 
 def calculate_payment():
@@ -42,4 +43,32 @@ def calculate_payment():
     return total
 
 
+def enough_money(drink_price, user_payment):
+    if drink_price < user_payment:
+        change = round(user_payment - drink_price, 2)
+        print(f"Here is your ${change} change. Enjoy!")
+        return True
+    elif drink_price > user_payment:
+        print(f"${user_payment} is not enough!")
+        return False
+    print("Enjoy!")
+    return True
+
+
+def make_drink(drink, ingredients):
+    for item in ingredients:
+        supplies[item] -= drink[item]
+    print(f"Enjoy your {drink}!")
+
+
 turned_on = True
+while turned_on:
+    your_selection = input("What would you like to drink?\n\t1. Coffee\n\t2. Tea")
+    if your_selection != "1" and your_selection != "2":
+        print("Please make a valid selection")
+    else:
+        your_drink = MENU[your_selection]
+        if enough_supplies(your_drink["ingredients"]):
+            your_payment = calculate_payment()
+            if enough_money(your_drink["price"], your_payment):
+                make_drink(your_drink["ingredients"], your_selection)
